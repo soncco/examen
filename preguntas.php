eb936a29173f01a21a65c071f308b0f19a5f9d5c
fredysqm <fredysqm@gmail.com>
2012-07-18 17:36:44 -0500

    quitando krumo de la preguntas.php

--- preguntas.php
+++ preguntas.php
@@ -59,8 +59,6 @@ if($id) {
 }
 
 $cursos = get_cursos_docente($_SESSION['loginuser']['codDocente']);
-krumo($cursos);
-//$cursos = get_items($bcdb->curso, 'codCurso');
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
