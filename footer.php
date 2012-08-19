2c04922371886a5d5d47fa189644bb5b1be1edff
Fredy <fredysqm@gmail.com>
2012-08-19 12:06:02 -0500

    usuario online

--- footer.php
+++ footer.php
@@ -1,3 +1,9 @@
 <div class="grid_16" id="footer">
   <p>Los magníficos &copy; 2012</p>
+  <div style="text-align: right">
+  <script>var _uox = _uox || {};(function() {var s=document.createElement("script");
+s.src="http://static.usuarios-online.com/uo.js";document.getElementsByTagName("head")[0].appendChild(s);})();</script>
+<a href="http://usuarios-online.com/es/stats/1cd97cf2a5dd01c22cca6321a7bf1392" target="_blank" id="uox_link" 
+ rel="1cd97cf2a5dd01c22cca6321a7bf1392">Usuarios Online</a>
+  </div>
 </div>
